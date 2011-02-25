815e0e847bcde5c224c66ed73d6e550a1c572679
AGAWA Koji <atty303@gmail.com>
2011-02-25 19:06:32 +0900

    Use template.

--- app/apps/juvisual/handlers.py
+++ app/apps/juvisual/handlers.py
@@ -15,37 +15,6 @@ from . import models
 
 class MainHandler(RequestHandler):
     def get(self):
-#        self.response.headers['Content-Type'] = 'text/html; charset=UTF-8'
-        res = ''
-        res += '<html><body><ul>'
-
         tunes = models.Tune.all().order('tune_id')
 
-        for tune in tunes:
-            res += '<li>'
-            if tune.title:
-                res += '<b>%s</b>' % tune.title
-            if tune.artist:
-                res += '/ %s' % tune.artist
-            res += '</li>'
-
-        # Write the submission form and the footer of the page
-        res += '</ul></body></html>'
-
-        return Response(res)
-
-        tvars = dict(tunes=tunes)
-        # path = os.path.join(os.path.dirname(__file__), 'main.html')
-        # self.response.out.write(template.render(path, tvars))
-
-
-class HelloWorldHandler(RequestHandler):
-    def get(self):
-        """Simply returns a Response object with an enigmatic salutation."""
-        return Response('Hello, World!')
-
-
-class PrettyHelloWorldHandler(RequestHandler):
-    def get(self):
-        """Simply returns a rendered template with an enigmatic salutation."""
-        return render_response('hello_world.html', message='Hello, World!')
+        return render_response('main.html', tunes=tunes)
